2c6e3dbf6d735cf1580a72f563fa283ecea07fc1
github username <github email address>
2018-12-18 14:32:19 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+
 	def index
 		@posts = Post.all
 	end
@@ -17,13 +18,16 @@ class PostsController < ApplicationController
 	  redirect_to post_path(@post)
 	end
 
+
+	def edit
+		 @post = Post.find(params[:id])
+	end
+
 	def update
 	  @post = Post.find(params[:id])
 	  @post.update(params.require(:post))
 	  redirect_to post_path(@post)
 	end
 
-	def edit
-	  @post = Post.find(params[:id])
-	end
+
 end
